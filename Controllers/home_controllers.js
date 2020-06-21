10f7f82e49468d1d42731dcb9be1f5dcc61ae545
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-06-21 10:14:12 +0530

    Add a router and controller for home

---
+++ Controllers/home_controllers.js
@@ -0,0 +1,6 @@
+//Format: module.exports.actionName = function(req,res){}
+
+module.exports.home=function(req,res){
+    return res.end('<h1>Express is up for Codeial</h1>')
+}
+
--- Routes/index.js
+++ Routes/index.js
@@ -1,9 +1,11 @@
 const express=require('express');
 
 const Router= express.Router();
+const homeController = require('../Controllers/home_controllers');
 
 //to check whether router is working or not
 console.log("Router loaded");
 
+Router.get('/',homeController.home);
 
 module.exports= Router;
