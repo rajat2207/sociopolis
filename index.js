80af35580e8f0447793ba280070e7deff96a0a63
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-06-27 13:31:05 +0530

    Setup scss

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore=require('connect-mongo')(session);
+//setting up sass middleware
+const sassMiddleware= require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    /* Options */
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix:  '/css'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
 
 //setting up middleware to read the post requests
 app.use(express.urlencoded());
