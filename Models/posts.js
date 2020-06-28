d8dc2128ef2919d3647132a27861d397966a31c1
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-06-29 02:39:31 +0530

    Setup comment Schema

--- Models/posts.js
+++ Models/posts.js
@@ -8,8 +8,12 @@ const postSchema= new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+    },
+    //include the array of all comments in this post itself
+    comments:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
     }
-
 },{
     timestamps: true
 })
