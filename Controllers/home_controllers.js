6758238986cfaf7e90a9df6356c61373ce9f8a54
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-06-29 00:17:40 +0530

    Display posts on home page alon with author

--- Controllers/home_controllers.js
+++ Controllers/home_controllers.js
@@ -1,10 +1,23 @@
+const Post=require('../Models/posts.js');
+
+
 //Format: module.exports.actionName = function(req,res){}
 
 module.exports.home=function(req,res) {
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{
-        'title': 'SocioPolis | Home'
+    
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title: 'SocioPolis | Home',
+    //         posts: posts
+    //     })
+    // })
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: 'SocioPolis | Home',
+            posts: posts
+        })
     })
 }
 
