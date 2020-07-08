74a164ca6c44b32f568c7728357aeaf35e2b33a4
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-07-09 01:06:59 +0530

    Set Up Directory Structure For API

---
+++ Routes/api/index.js
@@ -0,0 +1,8 @@
+const express=require('express');
+
+const Router= express.Router();
+
+Router.use('/v1',require('./v1'));
+
+
+module.exports= Router;
--- Routes/index.js
+++ Routes/index.js
@@ -14,5 +14,6 @@ Router.use('/users',require('./users'));
 Router.use('/posts',require('./posts'));
 Router.use('/comments',require('./comments'));
 
+Router.use('/api',require('./api'));
 
 module.exports= Router;
