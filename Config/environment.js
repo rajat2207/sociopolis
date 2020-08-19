764fc016c0d56974d97421214e01894367e63527
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-08-19 17:11:03 +0530

    Create Logger and handle errors

--- Config/environment.js
+++ Config/environment.js
@@ -1,16 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const logDirectory = path.join(__dirname, '../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const rfs = require('rotating-file-stream');
+
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d',
+  path: logDirectory,
+});
+
 const development = {
-  name: "development",
-  asset_path: "/assets",
-  session_cookie_key: "blahsomething",
-  db: "SocioPolis_development",
+  name: 'development',
+  asset_path: '/assets',
+  session_cookie_key: 'blahsomething',
+  db: 'SocioPolis_development',
   smtp: {
-    service: "gmail",
-    host: "smtp.gmail.com",
+    service: 'gmail',
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
-      user: "sociopolissite@gmail.com",
-      pass: "sociopolis123",
+      user: 'sociopolissite@gmail.com',
+      pass: 'sociopolis123',
     },
     tls: {
       // do not fail on invalid certs
@@ -18,20 +31,26 @@ const development = {
     },
   },
   google_client_id:
-    "1001043350512-2bkr91d6cb9o0ruov90lc0oufbftdru1.apps.googleusercontent.com",
-  google_client_secret: "a1ZtpTxJ10dB-Qx4aWYHW32u",
-  google_callback_URL: "http://localhost:8000/users/auth/google/callback",
-  jwt_secret_key: "sociopolis",
+    '1001043350512-2bkr91d6cb9o0ruov90lc0oufbftdru1.apps.googleusercontent.com',
+  google_client_secret: 'a1ZtpTxJ10dB-Qx4aWYHW32u',
+  google_callback_URL: 'http://localhost:8000/users/auth/google/callback',
+  jwt_secret_key: 'sociopolis',
+  morgan: {
+    mode: 'dev',
+    options: { stream: accessLogStream },
+  },
 };
 
+require('dotenv').config();
+
 const production = {
-  name: "production",
+  name: 'production',
   asset_path: process.env.SOCIOPOLIS_ASSETS_PATH,
   session_cookie_key: process.env.SOCIOPOLIS_SESSION_COOKIE,
   db: process.env.SOCIOPOLIS_DB,
   smtp: {
-    service: "gmail",
-    host: "smtp.gmail.com",
+    service: 'gmail',
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
@@ -47,11 +66,14 @@ const production = {
   google_client_secret: process.env.SOCIOPOLIS_GOOGLE_CLIENT_SECRET,
   google_callback_URL: process.env.SOCIOPOLIS_GOOGLE_CALLBACK_URL,
   jwt_secret_key: process.env.SOCIOPOLIS_JWT_SECRET,
+  morgan: {
+    mode: 'combined',
+    options: { stream: accessLogStream },
+  },
 };
 
-module.exports =
-  eval(process.env.SOCIOPOLIS_ENVIRONMENT) == undefined
-    ? development
-    : eval(process.env.SOCIOPOLIS_ENVIRONMENT);
+module.exports = eval(process.env.NODE_ENV == production)
+  ? development
+  : production;
 
 // module.exports = production;
--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 const express = require('express');
-const env=require('./Config/environment');
+const env = require('./Config/environment');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const port = 8000;
-const app =express();
+const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const db=require('./config/mongoose');
+const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const passportJWT=require('./Config/passport-jwt-strategy');
-const passportGoogle=require('./Config/passport-google-oauth2-strategy');
-const MongoStore=require('connect-mongo')(session);
+const passportJWT = require('./Config/passport-jwt-strategy');
+const passportGoogle = require('./Config/passport-google-oauth2-strategy');
+const MongoStore = require('connect-mongo')(session);
 //setting up sass middleware
-const sassMiddleware= require('node-sass-middleware');
-const flash=require('connect-flash');
-const customMWare= require('./Config/middleware');
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
-const path=require('path');
+const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMWare = require('./Config/middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const path = require('path');
 
 //setup the chat server to be used with socket.io
 const chatServer = require('http').Server(app);
-const chatSockets=require('./Config/chat_sockets').chatSockets(chatServer); 
+const chatSockets = require('./Config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
-console.log("chat server is listening on port 5000")
+console.log('chat server is listening on port 5000');
 
-app.use(sassMiddleware({
-    /* Options */
-    src: path.join(__dirname,env.asset_path,'scss'),
-    dest: path.join(__dirname,env.asset_path,'css'),
-    debug: true,
-    outputStyle: 'extended',
-    prefix:  '/css'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
-}));
+//sass Middleware should only run if the application is in development mode
+if (env.name == 'development') {
+  app.use(
+    sassMiddleware({
+      /* Options */
+      src: path.join(__dirname,env.asset_path,'scss'),
+      dest: path.join(__dirname,env.asset_path,'css'),
+      debug: true,
+      outputStyle: 'extended',
+      prefix: '/css', // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+    })
+  );
+}
 
 //setting up middleware to read the post requests
 app.use(express.urlencoded());
@@ -45,7 +51,10 @@ app.use(cookieParser());
 app.use(express.static(__dirname+env.asset_path));
 
 //make the upload path available to the browser
-app.use('/uploads',express.static('./uploads'));
+app.use('/uploads', express.static('./uploads'));
+
+//setting up the logger
+app.use(logger(env.morgan.mode, env.morgan.options));
 
 //use layouts
 app.use(expressLayouts);
@@ -54,32 +63,32 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-
-
 //setup view engine
-app.set('view engine','ejs');
-app.set('views','./Views');
+app.set('view engine', 'ejs');
+app.set('views', './Views');
 
 //mongo store is used to store the session cookie in the db
-app.use(session({
+app.use(
+  session({
     name: 'sociopolis',
     // TODO change the secret before deployment in production mode
     secret: env.session_cookie_key,
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000 * 60 * 100)
+      maxAge: 1000 * 60 * 100,
     },
     store: new MongoStore(
-        {
+      {
         mongooseConnection: db,
-        autoRemove: 'disabled'
-        },
-        function(err){
-            console.log(err || 'connect mongodb-store ok')
-        }
-    )
-}));
+        autoRemove: 'disabled',
+      },
+      function (err) {
+        console.log(err || 'connect mongodb-store ok');
+      }
+    ),
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -91,12 +100,12 @@ app.use(flash());
 app.use(customMWare.setFlash);
 
 //use Express Router
-app.use('/',require('./Routes'));
+app.use('/', require('./Routes'));
 
-app.listen(port,function(err){
-    if(err){
-        console.log(`There is error in running the server: ${err}` );
-    }
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`There is error in running the server: ${err}`);
+  }
 
-    console.log(`My server is running up and good on port: ${port}`);
-})
+  console.log(`My server is running up and good on port: ${port}`);
+});
